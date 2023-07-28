3fe15e95209f412e7ae9ffa24b1163994f8e2460
Haider <malikhaiderkhan81@gmail.com>
2023-07-28 15:36:13 +0500

    add test for homepage

--- src/--test--/homePage.test.js
+++ src/--test--/homePage.test.js
@@ -6,3 +6,25 @@ import Home from '../components/HomePage';
 import '@testing-library/jest-dom/extend-expect';
 
 
+describe('Home page component', () => {
+  it('should match DOM Snapshot', () => {
+    const tree = renderer
+      .create(
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the Homepage component correctly', () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
+    const welcome = screen.queryByText('Welcome to our page!');
+    expect(welcome).toBeInTheDocument();
+  });
+});
