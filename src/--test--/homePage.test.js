9248fa834548101a2850dd27daac53bdd360c4b7
Haider <malikhaiderkhan81@gmail.com>
2023-07-28 15:32:06 +0500

    import libraries for hompage test

--- src/--test--/homePage.test.js
+++ src/--test--/homePage.test.js
@@ -5,25 +5,4 @@ import { MemoryRouter } from 'react-router-dom';
 import Home from '../components/HomePage';
 import '@testing-library/jest-dom/extend-expect';
 
-describe('Home page component', () => {
-  it('should match DOM Snapshot', () => {
-    const tree = renderer
-      .create(
-        <MemoryRouter>
-          <Home />
-        </MemoryRouter>,
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
 
-  it('renders the Homepage component correctly', () => {
-    render(
-      <MemoryRouter>
-        <Home />
-      </MemoryRouter>,
-    );
-    const welcome = screen.queryByText('Welcome to our page!');
-    expect(welcome).toBeInTheDocument();
-  });
-});
